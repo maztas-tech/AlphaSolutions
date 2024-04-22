010aff9601f89b4ed556affba9d4cb680493f576
maztas-tech <mata0001@stud.kea.dk>
2024-04-22 11:23:10 +0200

    Initial commit

---
+++ src/main/java/project/alphasolutionsproject/AlphaSolutionsProjectApplication.java
@@ -0,0 +1,13 @@
+package project.alphasolutionsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaSolutionsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaSolutionsProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/alphasolutionsproject/AlphaSolutionsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.alphasolutionsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaSolutionsProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
