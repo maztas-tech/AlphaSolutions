da05db75dc756af41ea4bd5ce563e6e888a47ce3
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-04-26 12:39:48 +0200

    Initialized unit testing

---
+++ src/test/java/project/alphasolutionsproject/repository/TestRepositoryTest.java
@@ -0,0 +1,18 @@
+package project.alphasolutionsproject.repository;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
