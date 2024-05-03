eebfca57efb4a73a0a78cc9a4b972298abce8a58
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-05-03 12:47:00 +0200

    Changed name of file. Create empty updateProject method.

---
+++ src/main/java/project/alphasolutionsproject/repository/ProjectRepository.java
@@ -0,0 +1,12 @@
+package project.alphasolutionsproject.repository;
+
+import org.springframework.stereotype.Repository;
+import project.alphasolutionsproject.model.Project;
+
+@Repository
+public class ProjectRepository {
+
+    public void updateProject(Project project){
+
+    }
+}
