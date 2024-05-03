99560b05b6f16db4351eff90b82d29b847dae450
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-05-03 13:30:39 +0200

    Added dependency injection in projectcontroller class.

--- src/main/java/project/alphasolutionsproject/controller/ProjectController.java
+++ src/main/java/project/alphasolutionsproject/controller/ProjectController.java
@@ -1,13 +1,20 @@
 package project.alphasolutionsproject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import project.alphasolutionsproject.model.Project;
+import project.alphasolutionsproject.repository.ProjectRepository;
 import project.alphasolutionsproject.service.ProjectService;
 
 @Controller
 public class ProjectController {
 
     private ProjectService projectService;
+    private Project project;
 
-
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+        this.project = new Project();
+    }
 }
