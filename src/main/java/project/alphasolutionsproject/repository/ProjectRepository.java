075b0facbbedf2fdf5ad7dec944351f332df76b5
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-05-03 13:03:03 +0200

    Imported updated pom file. Added values that refers to the application properties in spring. Created the SQL statement

--- src/main/java/project/alphasolutionsproject/repository/ProjectRepository.java
+++ src/main/java/project/alphasolutionsproject/repository/ProjectRepository.java
@@ -1,12 +1,20 @@
 package project.alphasolutionsproject.repository;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import project.alphasolutionsproject.model.Project;
 
 @Repository
 public class ProjectRepository {
 
-    public void updateProject(Project project){
+    @Value("${spring.datasource.url}")
+    private String db_url;
+    @Value("${spring.datasource.username}")
+    private String db_user;
+    @Value("${spring.datasource.password}")
+    private String db_pwd;
 
+    public void updateProject(Project project){
+        String SQL = "UPDATE project SET projectName = ? WHERE projectID = ?";
     }
 }
