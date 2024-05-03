6807568c7fcba09f1d29228469d21349bb4929cf
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-05-03 13:51:38 +0200

    Added edit project html Made changed the name of the template folder to templates.

--- src/main/java/project/alphasolutionsproject/controller/ProjectController.java
+++ src/main/java/project/alphasolutionsproject/controller/ProjectController.java
@@ -2,12 +2,14 @@ package project.alphasolutionsproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.alphasolutionsproject.model.Project;
 import project.alphasolutionsproject.repository.ProjectRepository;
 import project.alphasolutionsproject.service.ProjectService;
 
 @Controller
+@RequestMapping("/landing")
 public class ProjectController {
 
     private ProjectService projectService;
@@ -16,5 +18,10 @@ public class ProjectController {
     public ProjectController(ProjectService projectService) {
         this.projectService = projectService;
         this.project = new Project();
+    }
+
+    @GetMapping("/edit_project")
+    public String edit(ModelMap model) {
+        return "edit_project";
     }
 }
