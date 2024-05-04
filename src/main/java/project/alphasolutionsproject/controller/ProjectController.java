80b2e344ac6c519a8558ee6acd23374f42d22ba4
saal0001 <saal0001@stud.kea.dk>
2024-05-04 14:58:31 +0200

    i made a create project method and a create_project.html documt. i also made the endpoints for the get and post method.

--- src/main/java/project/alphasolutionsproject/controller/ProjectController.java
+++ src/main/java/project/alphasolutionsproject/controller/ProjectController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import project.alphasolutionsproject.model.Project;
 import project.alphasolutionsproject.repository.ProjectRepository;
@@ -31,5 +33,17 @@ public class ProjectController {
     @GetMapping("/edit")
     public String edit(ModelMap model) {
         return "edit_project";
+    }
+
+    @GetMapping("/createProject")
+    public String createProjectForm(Model model){
+        model.addAttribute("projectObejct",new Project());
+        return "project_creation";
+    }
+
+    @PostMapping("/createProject")
+    public String createProject(@ModelAttribute("projectObject") Project project){
+        projectService.createProject(project);
+        return "redirect:/alphasolutions";
     }
 }
