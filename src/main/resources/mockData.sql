71e1eb06fba6f9edfb5539a7d8c586b186f10299
maztas-tech <144379432+maztas-tech@users.noreply.github.com>
2024-05-14 23:32:44 +0200

    Added additional data and adjusted the data to end with "_H2" so it is more obvious the user is in development mode.

--- src/main/resources/mockData.sql
+++ src/main/resources/mockData.sql
@@ -28,26 +28,31 @@ CREATE TABLE IF NOT EXISTS task(
     );
 
 -- DML
-INSERT INTO project(projectName, startDate, endDate)
+INSERT INTO project (projectName, startDate, endDate)
 VALUES
-    ('MockProject1', '2023-10-27', '2024-05-29'),
-    ('MockProject2', '2023-12-20', '2024-04-20'),
-    ('MockProject3', '2023-12-24', '2024-04-10');
+    ('MockProject1_H2', '2023-10-27', '2024-05-29'),
+    ('MockProject2_H2', '2023-12-20', '2024-04-20'),
+    ('MockProject3_H2', '2023-12-24', '2024-04-10');
+
 
 INSERT INTO subProject (subProjectName, startDate, endDate, projectID)
 VALUES
-    ('SubProject 1', '2024-01-15', '2024-02-28', 1),
-    ('SubProject 2', '2024-02-20', '2024-03-31', 1),
-    ('SubProject 3', '2024-03-25', '2024-04-30', 2),
-    ('SubProject 4', '2024-04-10', '2024-05-31', 2);
+    ('SubProject 1_H2', '2024-01-15', '2024-02-28', 1),
+    ('SubProject 2_H2', '2024-02-20', '2024-03-31', 1),
+    ('SubProject 3_H2', '2024-03-25', '2024-04-30', 2),
+    ('SubProject 4_H2', '2024-04-10', '2024-05-31', 2);
+
 
 INSERT INTO task (taskName, taskDescription, taskTimeEstimate, subProjectID)
 VALUES
-    ('Task 1', 'Complete initial analysis', 8, 1),
-    ('Task 2', 'Develop prototype', 16, 2),
-    ('Task 3', 'Test prototype functionality', 10, 1),
-    ('Task 4', 'Refine design based on feedback', 12, 2),
-    ('Task 5', 'Gather user requirements', 6, 3),
-    ('Task 6', 'Design user interface', 14, 3),
-    ('Task 7', 'Implement backend functionality', 20, 3),
-    ('Task 8', 'Perform integration testing', 10, 1);
+    ('Task 1_H2', 'Complete initial analysis_H2', 8, 1),
+    ('Task 2_H2', 'Develop prototype_H2', 16, 2),
+    ('Task 3_H2', 'Test prototype functionality_H2', 10, 1),
+    ('Task 4_H2', 'Refine design based on feedback_H2', 12, 2),
+    ('Task 5_H2', 'Gather user requirements_H2', 6, 3),
+    ('Task 6_H2', 'Design user interface_H2', 14, 3),
+    ('Task 7_H2', 'Implement backend functionality_H2', 20, 3),
+    ('Task 8_H2', 'Perform integration testing_H2', 10, 1),
+    ('Task 9_H2', 'Conduct user testing_H2', 8, 4),
+    ('Task 10_H2', 'Document project findings_H2', 10, 4),
+    ('Task 11_H2', 'Prepare final presentation_H2', 12, 4);
