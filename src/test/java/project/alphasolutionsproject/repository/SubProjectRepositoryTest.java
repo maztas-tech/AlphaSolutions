ac0fa0e2cb349e8261b8c9a7b989105573d74e6f
maztas-tech <mata0001@stud.kea.dk>
2024-05-23 12:14:06 +0200

    Commented out @Test variable in context load

--- src/test/java/project/alphasolutionsproject/repository/SubProjectRepositoryTest.java
+++ src/test/java/project/alphasolutionsproject/repository/SubProjectRepositoryTest.java
@@ -3,6 +3,7 @@ package project.alphasolutionsproject.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import project.alphasolutionsproject.model.Project;
 import project.alphasolutionsproject.model.SubProject;
 
